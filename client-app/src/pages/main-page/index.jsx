8d18ad90c46d641e6d6156f880ccdc24a7f76cc6
Stenis76 <stenqvist.dick@gmail.com>
2020-05-08 08:19:44 +0200

    added react pages and structure

---
+++ client-app/src/pages/main-page/index.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import "./styles.scss";
+
+const MainPage = () => {
+  return (
+    <div className="main-page">
+      <h1>Nu är du på Huvudsidan</h1>
+    </div>
+  );
+};
+
+export default MainPage;
---
+++ client-app/src/pages/welcome-page/index.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import "./styles.scss";
+
+const WelcomePage = () => {
+  return (
+    <div className="welcome-page">
+      <h1>Nu är du på båtsidan</h1>
+    </div>
+  );
+};
+
+export default WelcomePage;
