ab32801b9541694e26d25f146e468ebec3c034ff
Stenis76 <stenqvist.dick@gmail.com>
2020-05-11 10:23:18 +0200

    mainpage

---
+++ client-app/src/components/Header/index.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+import "./styles.scss";
+
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="avatar">En avatar</div>
+      <div className="settings">
+        ett kugghjul för sin profil eller för åtkomst till admin
+      </div>
+    </div>
+  );
+};
+
+export default Header;
--- client-app/src/pages/main-page/index.jsx
+++ client-app/src/pages/main-page/index.jsx
@@ -1,11 +1,17 @@
 import React from "react";
 
 import "./styles.scss";
+import Header from "../../components/Header";
 
 const MainPage = () => {
   return (
     <div className="main-page">
-      <h1>Nu är du på Huvudsidan</h1>
+      <Header />
+      <div className="sidebar">Kanske nån sidebar här</div>
+      <div className="forum">
+        <h1>Nu är du på Huvudsidan</h1>
+        <p>Här ligger alla foruminläggen</p>
+      </div>
     </div>
   );
 };
